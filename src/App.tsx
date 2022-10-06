116b96da526130ab2c59fc2bfc3665dcdae31c15
InnocentLi <vincentlieie@gmail.com>
2022-10-06 20:28:15 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+	let pageHTML = `
+		<div>
+			<a href="#">
+				<span>Jake</span>
+			</a>
+		</div>`;
+
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://cn.pornhub.com/video/search?search=pronhub"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+
         </a>
       </header>
     </div>
